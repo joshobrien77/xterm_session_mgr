3aa6b266c415ca3a871d5c4bb4b6398da2757397
Josh <joshobrien77@gmail.com>
2025-05-03 23:34:49 -0600

    kill it all again

--- config.py
+++
@@ -1,7 +0,0 @@
-import json
-from pathlib import Path
-
-def load_sessions(path: str = None):
-    """Load sessions JSON from the given path or default file.""""
-    path = Path(path or Path.home() / ".iterm2_sessions.json")
-    return json.loads(path.read_text())
